9650e9f7a35e40b23a43669743a11db3f495f899
Prokuma <prokuma@prokuma.dev>
2022-12-16 19:00:27 +0900

    using pointer

--- .interpreter.cpp
+++ .interpreter.cpp
@@ -1,58 +1,69 @@
 #include <bits/stdc++.h>
+#define MEM_SIZE 100000
 using namespace std;
 
-int main() {
+int compare(char *str1, string str2) {
+    for (int i = 0; i < 3; i++) {
+	if (str1[i] != str2.at(i)) {
+	    return 0;
+	}
+    }
+    return 1;
+}
+
+int main(int argc, char **argv) {
 
     // Initialization
-    int mem_size = 100000;
-    unsigned int ptr = 0;
-    vector<unsigned char> mem(mem_size, 0);
-    stack<int> loops;
+    int ptr = 0;
+    unsigned char mem[MEM_SIZE]; 
+    stack<char*> loops;
 
     // Load Source File
-    ifstream file(".source.c3");
-    stringstream buffer;
-    buffer << file.rdbuf();
-    string code = buffer.str();
-    int len = code.size()/3;
+    ifstream file;
+    if (argc < 2) file.open(".source.c3");
+    else file.open(argv[1]);
+    filebuf *pbuf =  file.rdbuf();
+    size_t size = pbuf->pubseekoff(0, file.end, file.in);
+    pbuf->pubseekpos(0, file.in);
+    char *buf = new char[size];
+    pbuf->sgetn(buf, size);
+    char *prg_end = &buf[size-1];
+    file.close();
 
     // interpreter Body
-    for (int prg=0; prg<len; prg++) {
-
-        // Get Order
-        string ord = code.substr(prg*3, 3);
-
+    for (char *prg = buf; prg <= prg_end; prg += 3) {
         // Modify Pointer
-        if (ord == "CCC") ptr = (ptr >= mem_size-1) ? 0 : ptr+1;
-        if (ord == "CC3") ptr = (ptr <= 0) ? mem_size-1 : ptr-1;
+        if (compare(prg, "CCC")) ptr = (ptr >= MEM_SIZE-1) ? 0 : ptr+1;
+	else if (compare(prg, "CC3")) ptr = (ptr <= 0) ? MEM_SIZE-1 : ptr-1;
 
         // Modify Memory
-        if (ord == "C3C") mem[ptr]++;
-        if (ord == "C33") mem[ptr]--;
+	else if (compare(prg, "C3C")) mem[ptr]++;
+	else if (compare(prg, "C33")) mem[ptr]--;
 
         // Input and Output
-        if (ord == "3CC") putchar(mem[ptr]);
-        if (ord == "3C3") mem[ptr] = getchar();
+	else if (compare(prg, "3CC")) putchar(mem[ptr]);
+	else if (compare(prg, "3C3")) mem[ptr] = getchar();
 
         // Loop Start
-        if (ord == "33C") {
+	else if (compare(prg, "33C")) {
             if (mem[ptr] != 0) loops.push(prg);
             else {
                 int depth = 1;
                 while (depth>0) {
-                    prg++;
-                    if (code.substr(prg*3, 3) == "33C") depth++;
-                    if (code.substr(prg*3, 3) == "333") depth--;
+                    prg+=3;
+                    if (compare(prg, "33C")) depth++;
+                    if (compare(prg, "333")) depth--;
                 }
             }
         }
 
         // Loop End
-        if (ord == "333") {
-            prg = loops.top()-1;
+	else if (compare(prg, "333")) {
+            prg = loops.top()-3;
             loops.pop();
         }
-
     }
 
+    delete[] buf;
+    return 0;
 }
